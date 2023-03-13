b7757d74f73d82736a1cb88ac1a7462cf8e9176b
Chandler Smith <smith.18.chandler@gmail.com>
2023-03-13 16:29:22 -0400

    Part 5, 6, and 7
    Adding code for part 5, 6, 7. Designed to run on Xcode Mac

---
+++ part_2/Task6.hpp
@@ -0,0 +1,15 @@
+//
+//  Task6.hpp
+//  part_2
+//
+//  Created by Chandler Smith on 3/10/23.
+//
+
+#ifndef Task6_hpp
+#define Task6_hpp
+
+#include <stdio.h>
+
+int virtualObject(cv::Mat filteredFrame, cv::Mat camMat, cv::Mat rvec, cv::Mat tvec,  cv::Mat distCoeff, std::vector<cv::Point2f> corner_Set);
+
+#endif /* Task6_hpp */
