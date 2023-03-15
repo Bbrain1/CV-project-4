af2e0bf84da62517e8b253361331d1ba25695b39
Chandler Smith <smith.18.chandler@gmail.com>
2023-03-15 13:28:46 -0400

    Update for part 1
    Update for Part 1 specifying for Mac

---
+++ Task3.cpp
@@ -0,0 +1,55 @@
+//
+//  Task3.cpp
+//  Project4
+//
+//  Created by Bennett Brain.
+
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+#include "Task3.h"
+#include <cstring>
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <dirent.h>
+#include <filesystem>
+
+// generate the initial camera matrix
+
+int genCamMat(cv::Mat& camMat, cv::Size frameSize) {
+
+	cv::Size cmSize = cv::Size(3, 3);
+	camMat = cv::Mat::zeros(cmSize, CV_64FC1);
+
+	camMat.at<double>(0, 0) = 1;
+	camMat.at<double>(0, 2) = frameSize.width / static_cast<double>(2);
+	camMat.at<double>(1, 1) = 1;
+	camMat.at<double>(1, 2) = frameSize.height / static_cast<double>(2);
+	camMat.at<double>(2, 2) = 1;
+	return 0;
+}
+
+int printMats(cv::Mat camMat, cv::Mat distMat){
+
+	std::string name = "";
+	printf("Enter your name: ");
+	std::cin >> name;
+	while (name == "")
+	{
+		printf("Invalid name. Please try again.\n");
+		printf("Enter your name: ");
+		std::cin >> name;
+	}
+
+    // LOOK HERE - CHANGE ABSOLUTE FILE NAME
+	std::string fileName = "/Users/chandlersmith/Desktop/CS5330/CV-project-4/part_2/part_2/" + name + "_calibration.json";
+
+	cv::FileStorage fs(fileName, cv::FileStorage::WRITE);
+	fs << "CameraMatrix" << camMat;
+	fs << "DistortionCoeffs" << distMat;
+
+	
+
+	return 0;
+}
